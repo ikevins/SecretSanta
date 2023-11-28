cbad4fce2ce7a92ba34972aba8493a2298b79840
Kevin Singh <singh.kevin98@gmail.com>
2023-11-27 23:00:40 -0500

    Update to UI; TODO: Finish generator algorithm

--- js/app.js
+++ js/app.js
@@ -1,48 +1,53 @@
 function generateInputs() {
     var container = document.getElementById("inputContainer");
-    container.innerHTML = "";
-
     var count = document.getElementById("participantCount").value;
+    container.innerHTML = "";
 
     for (var i = 0; i < count; i++) {
         var input = document.createElement("input");
         input.type = "text";
-        input.placeholder = "Participant " + (i + 1);
-        input.className = "participant-input";
+        input.placeholder = "Please enter participant #" + (i + 1) + "'s name here...";
+        input.className = "participantInput";
         container.appendChild(input);
     }
 }
 
 function generateSecretSanta() {
-    var inputs = document.getElementsByClassName("participant-input");
+    var inputs = document.getElementsByClassName("participantInput");
     var names = [];
 
     for (var input of inputs) {
         if (input.value.trim() !== '') {
             names.push(input.value.trim());
         }
+        else {
+            alert("You must have a name for each participant");
+            break;
+        }
     }
 
     var pairs = secretSanta(names);
+
     displayPairs(pairs);
+
 }
 
 function secretSanta(names) {
     let pairs = {};
-    let availableRecipients = [...names];
+    let participants = [...names];
 
     for (let giver of names) {
-        let index = Math.floor(Math.random() * availableRecipients.length);
-        let receiver = availableRecipients[index];
+        let index = Math.floor(Math.random() * participants.length);
+        let receiver = participants[index];
 
         // Ensure that the giver does not receive their own name
         while (giver === receiver) {
-            index = Math.floor(Math.random() * availableRecipients.length);
-            receiver = availableRecipients[index];
+            index = Math.floor(Math.random() * participants.length);
+            receiver = participants[index];
         }
 
         pairs[giver] = receiver;
-        availableRecipients.splice(index, 1); // Remove the chosen receiver
+        participants.splice(index, 1); // Remove the chosen receiver
     }
 
     return pairs;
@@ -58,4 +63,32 @@ function displayPairs(pairs) {
         p.appendChild(pairText);
         container.appendChild(p);
     }
+}
+
+
+// Functionality for participants input
+function increment() {
+    var value = parseInt(document.getElementById('participantCount').value);
+    if(isNaN(value)) {
+        value = 3
+    }
+    else {
+        value++;
+    }
+    document.getElementById('participantCount').value = value;
+    generateInputs()
+}
+function decrement() {
+    var value = parseInt(document.getElementById('participantCount').value);
+    if(isNaN(value)) {
+        value = 3;
+    }
+    else if(value > 3) {
+        value--;
+    }
+    else {
+        value = 3;
+    }
+    document.getElementById('participantCount').value = value;
+    generateInputs()
 }
